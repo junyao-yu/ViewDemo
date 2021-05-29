769a9dbbe535970d5ec5ae002012e4709235e822
yujunyao <yujunyao@yonglibao.com>
2021-05-29 22:53:41 +0800

    x

--- app/src/main/java/com/xinrenlei/javademo/behavior/CustomBehavior.java
+++ app/src/main/java/com/xinrenlei/javademo/behavior/CustomBehavior.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * Auth：yujunyao
@@ -45,29 +47,42 @@ public class CustomBehavior extends CoordinatorLayout.Behavior<TextView> {
     @Override
     public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull TextView child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
 
-        int consumeDy;
-        int offset = mOffsetTopAndBottom - dy;
-        int minOffset = -getChildScrollRange(child);
-        int maxOffset = 0;
 
-        if (offset < minOffset) {
-            offset = minOffset;
-        } else {
-            if (offset > maxOffset) {
-                offset = maxOffset;
+        if (target instanceof RecyclerView) {
+            RecyclerView mRecyclerView = (RecyclerView) target;
+            LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+
+            int firstPosition = layoutManager.findFirstCompletelyVisibleItemPosition();
+
+            if (firstPosition == 0) {//如果不判断的话，往下滑动的时候头部会先滑动出来，不过主要还是要看需要实现的效果
+                int consumeDy;
+                int offset = mOffsetTopAndBottom - dy;
+                int minOffset = -getChildScrollRange(child);
+                int maxOffset = 0;
+
+                if (offset < minOffset) {
+                    offset = minOffset;
+                } else {
+                    if (offset > maxOffset) {
+                        offset = maxOffset;
+                    }
+                }
+
+                System.out.println("offset--->" + offset);
+                System.out.println("child.getTop--->" + child.getTop());
+
+                ViewCompat.offsetTopAndBottom(child, offset - (child.getTop() - mLayoutTop));
+
+                consumeDy = mOffsetTopAndBottom - offset;
+
+                mOffsetTopAndBottom = offset;
+
+                consumed[1] = consumeDy;//把移动的距离传给父类
             }
         }
 
-        System.out.println("offset--->" + offset);
-        System.out.println("child.getTop--->" + child.getTop());
 
-        ViewCompat.offsetTopAndBottom(child, offset - (child.getTop() - mLayoutTop));
 
-        consumeDy = mOffsetTopAndBottom - offset;
-
-        mOffsetTopAndBottom = offset;
-
-        consumed[1] = consumeDy;//把移动的距离传给父类
     }
 
     //获取childView最大滑动距离
